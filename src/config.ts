24c06a5442c23b180e06a7d421f4e9214a46b865
Knut Melvær <knut@sanity.io>
2022-11-06 15:25:05 -0800

    content updates

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Developers 4 Editors.";
+export const SITE_DESCRIPTION = `Don't let Developer Experience hold editors back!`;
